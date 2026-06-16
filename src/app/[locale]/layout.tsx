31a45505625e3958694bc96df83c71d724d0e169
zironpro <dev@zironpro.ae>
2026-06-16 12:40:15 +0530

    feat: implement localized root layout with pixel metadata, schema markup, and tracking scripts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
+import Script from "next/script";
 
 import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
@@ -126,6 +127,29 @@ export default async function RootLayout({
 				{/* <Script id="video-schema" type="application/ld+json">
 					{JSON.stringify(videoJsonLd)}
 				</Script> */}
+				<Script id="facebook-pixel" strategy="afterInteractive">
+					{`
+						!function(f,b,e,v,n,t,s)
+						{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+						n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+						if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+						n.queue=[];t=b.createElement(e);t.async=!0;
+						t.src=v;s=b.getElementsByTagName(e)[0];
+						s.parentNode.insertBefore(t,s)}(window, document,'script',
+						'https://connect.facebook.net/en_US/fbevents.js');
+						fbq('init', '2267491257388701');
+						fbq('track', 'PageView');
+					`}
+				</Script>
+				<noscript>
+					<img
+						alt="facebook-pixel-noscript"
+						height="1"
+						src="https://www.facebook.com/tr?id=2267491257388701&ev=PageView&noscript=1"
+						style={{ display: "none" }}
+						width="1"
+					/>
+				</noscript>
 			</head>
 			<body
 				className={cn(
